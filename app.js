6d92304d10aca2daba566027405bce16a8c7b29f
Anirudh <anirudhrautela9906@gmail.com>
2024-02-26 18:34:27 +0530

    third commit

--- app.js
+++ app.js
@@ -33,11 +33,16 @@ app.use('/api/v1',order)
 
 
 
-app.use(express.static(path.join(__dirname, "./frontend/build"))) 
+// app.use(express.static(path.join(__dirname, "./frontend/build"))) 
 
-app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-})
+// app.get("*", (req,res) => {
+//     res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
+// })
+
+app.use(express.static('frontend/build'));
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname , "frontend/build", "index.html"));
+});
 
 // Middleware for Errors
 
