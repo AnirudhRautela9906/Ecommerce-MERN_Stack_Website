3cf542c0054dbc678e570e8cf64b32ba6ae3a4b1
Anirudh <anirudhrautela9906@gmail.com>
2024-02-26 17:48:43 +0530

    third commit

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 
 const connectDatabase = ()=>{
-if(process.env.NODE_ENV !== "production"){
-    mongoose.connect(process.env.DB_URI_LOCAL).then((data)=>{
-    console.log(`Mongodb connected with server: ${data.connection.host}`)
-})
-}
-else
+// if(process.env.NODE_ENV !== "production"){
+//     mongoose.connect(process.env.DB_URI_LOCAL).then((data)=>{
+//     console.log(`Mongodb connected with server: ${data.connection.host}`)
+// })
+// }
+// else
 {mongoose.connect(process.env.DB_URI_ONLINE).then((data)=>{
     console.log(`Mongodb connected with server: ${data.connection.host}`)
 })}
